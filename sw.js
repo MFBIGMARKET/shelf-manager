77d0e3f60e055906957b67d18bd7b5d91f55a057
MFBIGMARKET <mfbigmarketiritty@gmail.com>
2026-05-02 17:38:58 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,10 @@
-const CACHE_NAME = 'shelf-manager-v1';
+const CACHE_NAME = 'shelf-manager-v2';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 // Install Service Worker
